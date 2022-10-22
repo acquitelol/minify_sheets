40daefd22074d1a891e909c2591265eb7faa7b55
Acquite <acquitefx@gmail.com>
2022-10-22 17:08:02 +0100

    ver 1.0.0 (stable - ghost patch - removed unnecessary elements)

--- src/utils/format-text.ts
+++
@@ -1,6 +0,0 @@
-
-const formatString = text => {
-    return text.split("_").map(e => e[0].toUpperCase() + e.slice(1)).join(' ')
-}
-
-export { formatString };
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,4 @@
-import { translateString } from "./translate";
-import { formatString } from './format-text'
 import { debugInfo } from './debug'
 import { clipboardToast } from "./clipboard";
-import { external_plugins } from './external'
 
-export { translateString, formatString, debugInfo, clipboardToast, external_plugins };
+export { debugInfo, clipboardToast };
